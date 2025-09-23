da37a41985960f2e059005ce4e5c52238dd6f921
jaylinjjjunious <228570367+jaylinjjjunious@users.noreply.github.com>
2025-09-23 03:08:06 -0700

    Added FitSnacks PWA build

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'fitsnacks-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './assets/carousel-1.jpg',
+  './assets/carousel-2.jpg',
+  './assets/carousel-3.jpg',
+  './assets/product-funyuns.jpg',
+  './assets/product-chesters.jpg',
+  './assets/product-cheetos.jpg',
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
